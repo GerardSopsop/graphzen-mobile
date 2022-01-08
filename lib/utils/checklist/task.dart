c849d1ddb1efc14676ba2f3f3cd59e42ab025837
Jaumsss <thegreat.gerardsopsop@gmail.com>
2022-01-08 16:45:54 +0800

    feat: added attributes 'type' and 'attrs' to class Task

--- lib/utils/checklist/task.dart
+++ lib/utils/checklist/task.dart
@@ -1,7 +1,11 @@
+enum Type { BASIC, PARENT_UNIVERSAL, PARENT_EXISTENSIAL }
+
 class Task {
   String id;
+  final Type type;
   String label;
   List<Task> children;
+  Map<String, String> attrs;
 
-  Task(this.id, this.label, this.children);
+  Task(this.id, this.type, this.label, this.children, this.attrs);
 }
