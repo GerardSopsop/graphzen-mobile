805af87aa986d1cf59334aeb1bc092accac3e59c
Jaumsss <thegreat.gerardsopsop@gmail.com>
2022-01-08 17:40:27 +0800

    feat: added attributes to Checklist class

--- lib/utils/checklist/checklist.dart
+++ lib/utils/checklist/checklist.dart
@@ -3,10 +3,21 @@ import 'progress.dart';
 
 class Checklist {
   String id;
-  String name;
   String version;
   List<Task> tasks;
   Progress progress;
+  String creator;
+  String sign;
+  int last_modified;
+  bool is_edit_mode;
+  String snapshots;
+  Map<String, dynamic> metadata;
 
-  Checklist(this.id, this.name, this.version, this.tasks, this.progress);
+  Checklist(this.id, this.version, this.tasks, this.progress, this.creator,
+      this.sign, this.last_modified, this.is_edit_mode, this.snapshots,
+      {this.metadata = const {
+        'owner': '',
+        'name': '',
+        'blacklist': {'key': '', 'since': ''}
+      }});
 }
