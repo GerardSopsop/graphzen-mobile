51638928a5d8e5d098cde53d46fa073cd65a26c7
Jaumsss <thegreat.gerardsopsop@gmail.com>
2022-01-08 17:10:55 +0800

    fix: altered 'taskId' attribute of Progress class

--- lib/utils/checklist/progress.dart
+++ lib/utils/checklist/progress.dart
@@ -1,5 +1,13 @@
 class Progress {
-  String taskId;
+  Map<String, dynamic> taskId;
 
-  Progress(this.taskId);
+  Progress(
+      {this.taskId = const {
+        'value': 0,
+        'alias': '',
+        'signer': '',
+        'sign': '',
+        'date': 0,
+        'signed_version': ''
+      }});
 }
